c10b694aa10df95d98c8895a7705c1dd2d4e0ebc
mehtalllll <mmh6647047@gmail.com>
2025-01-02 22:22:22 +0330

    complete app

--- app/(layout)/payment-success/page.tsx
+++ app/(layout)/payment-success/page.tsx
@@ -6,7 +6,7 @@ const PaymentSuccessPage: React.FC = () => {
     <div className="p-6 container text-center">
       <TiTick className="w-full text-center text-green-500" size={50} />
       <h1 className="text-xl font-bold mb-4">پرداخت موفق</h1>
-      <p>سفارش شما با موفقیت ثبت شد. از خرید شما متشکریم!</p>
+      <p>سفارشش شما با موفقیت ثبت شد. از خرید شما متشکریم!</p>
     </div>
   );
 };
